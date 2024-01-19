580df072ff363a344525862670f8c99c87f32635
NathanHandley <nathanhandley@protonmail.com>
2024-01-19 08:40:23 -0600

    Add some Jeeves drops

---
+++ data/sql/updates/db_world/2024_01_19_00c.sql
@@ -0,0 +1,4 @@
+-- Add some Jeeves drops
+INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES (29724, 49050, 0, 0.2, 0, 1, 0, 1, 1, 'Library Guardian - Schematic: Jeeves');
+INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES (33432, 49050, 0, 0.9, 0, 1, 0, 1, 1, 'Leviathan Mk II - Schematic: Jeeves');
+INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES (33113, 49050, 0, 0.4, 0, 1, 0, 1, 1, 'Flame Leviathan - Schematic: Jeeves');
