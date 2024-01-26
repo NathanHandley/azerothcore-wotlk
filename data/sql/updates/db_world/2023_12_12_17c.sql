0de60380dcae4fb33b20a8bf7b816afd7ae46a5e
NathanHandley <nathanhandley@protonmail.com>
2024-01-26 08:31:49 -0600

    Moved custom database scripts to the custom folder

--- data/sql/updates/db_world/2023_12_12_17c.sql
+++
@@ -1,8 +0,0 @@
--- Fix times on Stranglethorn Fishing Extravaganza
-UPDATE game_event SET start_time = '2016-12-23 01:00:00' WHERE evententry = 14;
-UPDATE game_event SET start_time = '2023-12-24 13:00:00' WHERE eventEntry = 62;
-UPDATE game_event SET start_time = '2023-12-24 14:00:00' WHERE eventEntry = 15;
-UPDATE game_event SET start_time = '2023-12-24 14:00:00' WHERE eventEntry = 90;
--- Fix times on Kalu'ak Fishing Derby
-UPDATE game_event SET start_time = '2023-12-23 14:00:00' WHERE eventEntry = 63;
-UPDATE game_event SET start_time = '2023-12-23 14:00:00' WHERE eventEntry = 64;
--- data/sql/updates/db_world/2024_01_06_00c.sql
+++
@@ -1,5 +0,0 @@
--- Increase Required XP from 80-83
-DELETE FROM player_xp_for_level WHERE LEVEL > 79;
-INSERT INTO player_xp_for_level (Level, Experience) VALUES (80, 16708000);
-INSERT INTO player_xp_for_level (Level, Experience) VALUES (81, 33416000);
-INSERT INTO player_xp_for_level (Level, Experience) VALUES (82, 66832000);
