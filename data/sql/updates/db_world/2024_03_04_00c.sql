6dfd23b0e8e9cd556644aa7f6133929f3e3c8b65
NathanHandley <nathanhandley@protonmail.com>
2024-03-04 15:26:26 -0600

    Set otherwise zero sell price enchanting goods to 1 copper
    Allows selling of trade goods to a vendor

---
+++ data/sql/updates/db_world/2024_03_04_00c.sql
@@ -0,0 +1,2 @@
+-- Give enchanting trade goods at least a 1 copper sell price so they can be sold to vendors
+UPDATE item_template SET SellPrice = 1 WHERE class = 7 AND subclass = 12 AND SellPrice = 0;
