3606cdb30339f43d6774ddbf3691af7302474628
NathanHandley <nathanhandley@protonmail.com>
2024-03-20 15:27:22 -0500

    Further update raven lord drop rate

--- data/sql/updates/db_world/2024_03_20_00c.sql
+++ data/sql/updates/db_world/2024_03_20_00c.sql
@@ -1,2 +1,2 @@
--- Raise Reins of the Raven Lord drop rate to 1.5
-UPDATE creature_loot_template SET Chance = 1.5 WHERE Item = 32768;
+-- Raise Reins of the Raven Lord drop rate to 2
+UPDATE creature_loot_template SET Chance = 2 WHERE Item = 32768;
