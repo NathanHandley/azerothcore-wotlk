65eab94e958a74f122a772f16029af50825ba9ab
NathanHandley <nathanhandley@protonmail.com>
2024-03-23 11:57:26 -0500

    Remove Panic Peroidoc from Coilfang Striders

--- data/sql/updates/db_world/2024_03_23_01c.sql
+++ data/sql/updates/db_world/2024_03_23_01c.sql
@@ -1,2 +1,4 @@
 -- Remove unsealed chest conditions on LK25 Heroic
 DELETE FROM conditions WHERE sourceEntry = 51315 AND SourceGroup = 39168;
+-- Remove Panic Peroidoc from Coilfang Striders
+DELETE FROM creature_template_addon WHERE entry = 22056;
