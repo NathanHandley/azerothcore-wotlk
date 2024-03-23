63052e6f3927fa5c59f103a92aaedaee42168fe7
NathanHandley <nathanhandley@protonmail.com>
2024-03-23 13:38:30 -0500

    Correct the time for the Kalu'ak Fishing Derby

--- data/sql/updates/db_world/2024_03_23_01c.sql
+++ data/sql/updates/db_world/2024_03_23_01c.sql
@@ -4,3 +4,5 @@ DELETE FROM conditions WHERE sourceEntry = 51315 AND SourceGroup = 39168;
 DELETE FROM creature_template_addon WHERE entry = 22056;
 -- Disable root on Tainted Core
 UPDATE item_template SET spellid_3 = 0, spelltrigger_3 = 0 WHERE entry = 31088;
+-- Correct the time for the Kalu'ak Fishing Derby
+UPDATE game_event SET start_time = '2024-03-16 14:00:00' WHERE eventEntry IN (63, 64);
