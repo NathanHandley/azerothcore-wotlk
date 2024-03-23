80b3410dda0608a80c9dd8ed9753b07a35647e95
NathanHandley <nathanhandley@protonmail.com>
2024-03-23 12:04:58 -0500

    Disable root on picking up a Tainted Core

--- data/sql/updates/db_world/2024_03_23_01c.sql
+++ data/sql/updates/db_world/2024_03_23_01c.sql
@@ -2,3 +2,5 @@
 DELETE FROM conditions WHERE sourceEntry = 51315 AND SourceGroup = 39168;
 -- Remove Panic Peroidoc from Coilfang Striders
 DELETE FROM creature_template_addon WHERE entry = 22056;
+-- Disable root on Tainted Core
+UPDATE item_template SET spellid_3 = 0, spelltrigger_3 = 0 WHERE entry = 31088;
