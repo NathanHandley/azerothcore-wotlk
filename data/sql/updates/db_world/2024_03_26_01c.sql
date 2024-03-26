44b4d8270f418c3747193ece1d9780e0b5223a58
NathanHandley <nathanhandley@protonmail.com>
2024-03-26 12:24:27 -0500

    Update 2024_03_26_01c.sql
    Flatten EXP curve 81+

--- data/sql/updates/db_world/2024_03_26_01c.sql
+++ data/sql/updates/db_world/2024_03_26_01c.sql
@@ -1,12 +1,12 @@
 -- Level through 90
 DELETE FROM player_xp_for_level WHERE LEVEL > 79;
 INSERT INTO player_xp_for_level (Level, Experience) VALUES (80, 15000000);
-INSERT INTO player_xp_for_level (Level, Experience) VALUES (81, 20000000);
-INSERT INTO player_xp_for_level (Level, Experience) VALUES (82, 25000000);
+INSERT INTO player_xp_for_level (Level, Experience) VALUES (81, 30000000);
+INSERT INTO player_xp_for_level (Level, Experience) VALUES (82, 30000000);
 INSERT INTO player_xp_for_level (Level, Experience) VALUES (83, 30000000);
-INSERT INTO player_xp_for_level (Level, Experience) VALUES (84, 35000000);
-INSERT INTO player_xp_for_level (Level, Experience) VALUES (85, 40000000);
-INSERT INTO player_xp_for_level (Level, Experience) VALUES (86, 45000000);
-INSERT INTO player_xp_for_level (Level, Experience) VALUES (87, 50000000);
-INSERT INTO player_xp_for_level (Level, Experience) VALUES (88, 55000000);
-INSERT INTO player_xp_for_level (Level, Experience) VALUES (89, 60000000);
+INSERT INTO player_xp_for_level (Level, Experience) VALUES (84, 30000000);
+INSERT INTO player_xp_for_level (Level, Experience) VALUES (85, 30000000);
+INSERT INTO player_xp_for_level (Level, Experience) VALUES (86, 30000000);
+INSERT INTO player_xp_for_level (Level, Experience) VALUES (87, 30000000);
+INSERT INTO player_xp_for_level (Level, Experience) VALUES (88, 30000000);
+INSERT INTO player_xp_for_level (Level, Experience) VALUES (89, 30000000);
