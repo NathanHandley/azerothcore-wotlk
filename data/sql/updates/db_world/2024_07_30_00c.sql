174ce2c6ad3e5b91771c45e301d4d3ae9dfa694a
NathanHandley <nathanhandley@protonmail.com>
2024-07-30 18:44:34 -0500

    SQL fixes for recent upstream changes

---
+++ data/sql/updates/db_world/2024_07_30_00c.sql
@@ -0,0 +1,3 @@
+-- Model for legendary bag merchant
+DELETE FROM `creature_template_model` WHERE `CreatureID` = 83000;
+INSERT INTO `creature_template_model` (`CreatureID`, `Idx`, `CreatureDisplayID`, `DisplayScale`, `Probability`, `VerifiedBuild`) VALUES (83000, 0, 18148, 1, 1, 0);
