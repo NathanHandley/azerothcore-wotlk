e5a5179f77544d48a91f01d427413303f8708315
NathanHandley <nathanhandley@protonmail.com>
2024-09-18 15:16:48 -0500

    Correct text on Rainbow Rivets

--- data/sql/updates/db_world/2024_09_18_00c.sql
+++ data/sql/updates/db_world/2024_09_18_00c.sql
@@ -1,2 +1,4 @@
 -- Make most items non-unique
 UPDATE item_template SET maxcount = 0 WHERE maxcount = 1 AND bonding < 4 AND (BagFamily & 256 = FALSE) AND startquest = 0 AND (Flags & 2 = FALSE) AND class != 12;
+-- Fix text on rainbow rivets item
+UPDATE item_template SET DESCRIPTION = 'Teaches you how to make Rainbow Rivets' WHERE entry = 81104;
